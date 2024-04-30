c8514fca4644ee508c522cd0c1ece6f769e7bfa9
jsum01 <djadjawltjr@gmail.com>
2024-04-30 23:07:17 +0900

    Update Main-Sidebar: Create side bar visiable state

--- portfolium/src/Main.tsx
+++ portfolium/src/Main.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Display from "./components/Display";
 import SideBar from "./components/SideBar";
 import styles from "./Main.module.css";
@@ -5,11 +6,13 @@ import styles from "./Main.module.css";
 
 export default function Main() {
 
+  const [visiableSideBar, setVisiableSideBar] = useState(false);
 
-    return (
-        <div className={styles.body}>
-            <SideBar />
-            <Display />
-        </div>
-    )
+
+  return (
+    <div className={styles.body}>
+      {visiableSideBar ? <SideBar /> : null }
+        <Display />
+    </div>
+  )
 }
