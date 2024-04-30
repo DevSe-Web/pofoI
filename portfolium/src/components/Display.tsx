f7e573b63770f527711c314518e4627d99a60159
jsum01 <djadjawltjr@gmail.com>
2024-04-30 23:11:57 +0900

    Update Display: Optimize background-image

--- portfolium/src/components/Display.tsx
+++ portfolium/src/components/Display.tsx
@@ -1,17 +1,31 @@
 import TopNavBar from "./TopNavBar";
 import styles from "./Display.module.css";
-import background_img from "../images/_.gif";
+import Text from "./Welcome/Text";
+import Profile from "./ProfileModule/Profile";
+import { useState, useEffect } from "react";
 
 
 
 const Display = () => {
+
+  const [viewActive, setViewActive] = useState(false);
+
+  useEffect(() => {
+    setViewActive(true)
+  }, [])
+
   return (
     <div className={styles.body}>
-      <div className={styles.background}>
+      <div className={styles.background_container}></div>
+      <div className={styles.view_container}>
         <TopNavBar />
-        <img src={background_img} alt="" className={styles.ls}/>
+        <div className={styles.view_welcome_text}>
+          <Text />
+        </div>
+        <div className={`${styles.view_profile} ${viewActive && styles.active}`}>
+          <Profile />
+        </div>
       </div>
-
     </div>
   )
 }
