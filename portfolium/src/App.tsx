7a9acc64b3b47677b5e7b88ea3553991ef7a9aa8
jsum01 <djadjawltjr@gmail.com>
2024-05-06 20:10:19 +0900

    Update Firebase Connecting & init login component

--- portfolium/src/App.tsx
+++ portfolium/src/App.tsx
@@ -1,7 +1,29 @@
 import Router from './Router';
 import './App.css';
+import { useEffect } from 'react';
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
 
 function App() {
+
+  // [Debug]Checking code - Firebase Connecting
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "1"));
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, doc.data());
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
   return (
     <div>
       <Router />
